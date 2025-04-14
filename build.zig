c022c1a119690485fb3902b7d7c0f59da05ca225
マリウス <marius@xn--gckvb8fzb.com>
2025-04-14 13:50:57 -0500

    Replace createModule with addModule

--- build.zig
+++ build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const lib_mod = b.createModule(.{
+    const lib_mod = b.addModule("bfstree", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
